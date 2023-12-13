b7f229ac6aa6c34597093250a1e4b14ad0ae0149
mconiaris <mconiaris@gmail.com>
2023-12-13 15:07:44 -0500

    Added new and create actions in ItemsController.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,18 @@ class ItemsController < ApplicationController
 	def show
 		@item = Item.find(params[:id])
 	end
+
+	def new
+		@item = Item.new
+	end
+
+	def create
+		@item = Item(item_params)
+
+		if @item.save
+      redirect_to @item, status: :unprocessable_entity
+    else
+      render :new
+    end
+	end
 end
