9da3cdd2f8b43fa39c2777dfb0e456bb6d334702
mconiaris <mconiaris@gmail.com>
2023-12-13 16:05:45 -0500

    Added destroy action to items controller.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,13 @@ class ItemsController < ApplicationController
 		end
 	end
 
+	def destroy
+		@item = Item.find(params[:id])
+		@item.destroy
+
+		redirect_to root_path, status: :see_other
+	end
+
 	private
 		def item_params
 			params.require(:item).permit(:name)
