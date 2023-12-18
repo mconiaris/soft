0b495b6667ec69439df80fa305cce640cfcf155a
mconiaris <mconiaris@gmail.com>
2023-12-18 11:36:53 -0500

    Created route for removed index.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'items#index'
 
   resources :items
+  get "/removed", to: "removed#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
