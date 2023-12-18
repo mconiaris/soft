4851498e2d147c38bc3738ef9a83432674ae18f0
mconiaris <mconiaris@gmail.com>
2023-12-18 11:30:52 -0500

    Changed Item scope to unscoped in Removed index. Refactored destroy method to implement restore method.

--- app/controllers/removed_controller.rb
+++ app/controllers/removed_controller.rb
@@ -1,2 +1,17 @@
 class RemovedController < ApplicationController
+def index
+		@items = Item.unscoped
+	end
+
+	def destroy
+		@item = Item.find(params[:id])
+		@item.restore
+
+		redirect_to root_path, status: :see_other
+	end
+
+	private
+		def item_params
+			params.require(:item).permit(:name)
+		end
 end
