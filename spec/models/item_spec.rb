721efc6ab04da57633cfe2e4cbf612763a2897b9
mconiaris <mconiaris@gmail.com>
2023-12-19 12:19:08 -0500

    Created rspec test for Item creation.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:context) do
+    @item = Item.new(name: "Computer")
+  end
+
+  context "After instanciation" do
+    it "should be an Item" do
+      expect(@item).to be_an_instance_of(Item)
+    end
+    it "should be named 'Computer'" do
+      expect(@item.name).to eq("Computer")
+    end
+    it "should have a 'deleted_at' value of nil" do
+      expect(@item.deleted_at).to eq(nil)
+    end
+  end
+
 end
